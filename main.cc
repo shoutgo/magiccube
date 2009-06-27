522a8b1f8afa87e798fd2685e8289cf1921bd32e
Yue Peng <yp@localhost.localdomain>
2009-06-27 12:07:35 +0800

    draft2

--- main.cc
+++ main.cc
@@ -1,18 +1,17 @@
 
-#include "main.h"
-
 use namespace std;
 
-z        
-|    
-|     y
-|   /
-| /
-0-------- x
+/*************************
+       zh        
+       |     yh
+       |   /
+       | /
+       o------ xh
+    xl,yl,zl
 
-    yh
- xl zl xh zh
-    yl
+       yh
+    xl zl xh zh
+       yl
 
        6 7 8
        3 4 5
@@ -23,25 +22,44 @@ z
        0 1 2
        3 4 5
        6 7 8
+ *************************/
 
 const enum COLOR	{R,G,B,K,T,V,W};
 const enum AXIS		{X,Y,Z};
 const enum POS		{LOW,HIGH};
-const enum DIR		{BACKWARD,ONFACE,FORWARD};
+const enum DIRC		{BACKWARD,OPPISITE,FORWARD};
 
 class MAGICCUBE 
 {
 public:
+	MAGICCUBE* father;
 	AXIS axis;
 	POS  pos;
-	DIR  dir;
-	int  itr;
-	MAGICCUBE* father;
+	DIRC dirc;
 public:
 	COLOR xl[9],xh[9],yl[9],yh[9],zl[9],zh[9];
 public:
+	void
+	init(){
+	};
+
+	void
+	random(){
+	};
+
+	bool
+	ok1(COLOR *c){
+		return (c[0]==c[4] && c[1]==c[4] && c[2]==c[4] && c[3]==c[4]\
+		     && c[5]==c[4] && c[6]==c[4] && c[7]==c[4] && c[8]==c[4]);
+	};
+
+	bool 
+	ok6(){
+		return (ok1(xl) && ok1(xh) && ok1(yl) && ok1(yh) && ok1(zl) && ok1(zh));
+	};
+
 	MAGICCUBE
-	rot(AXIS ax, POS p, DIR d){
+	rot(AXIS ax, POS p, DIRC d){
 		MAGICCUBE tmp=*this;
 		switch ax {
 			case X:
@@ -72,55 +90,42 @@ public:
 						};
 				};
 		};
-};
-	bool
-	ok(COLOR *c){
-		return (c[0]==c[4] && c[1]==c[4] && c[2]==c[4] && c[3]==c[4]\
-		     && c[5]==c[4] && c[6]==c[4] && c[7]==c[4] && c[8]==c[4]);
-};
-	bool 
-	successed(){
-		return (ok(xl) && ok(xh) && ok(yl) && ok(yh) && ok(zl) && ok(zh));
-};
+	};
+	
+	void
+	act(MAGICCUBE mc){
+		for (AXIS ax=X;;++ax){
+			if ax==father->axis && father->axis==father->father->axis
+				continue;
+			for (POS p=LOW;;++p){
+				if ax==father->axis && p==father->pos
+					continue;
+				for (DIRC d=BACKWARD;;++d){
+					newmc = mc.rot(ax,p,d);
+					if newmc.ok6()
+						newmc.print();
+					else
+						act(newmc);		
+				}
+			}
+		}
+	};
+
 	void
 	print(){
 		father->print();
 		...
-};
-};
-
-MAGICCUBE
-random(MAGICCUBE mc){};
-
-void
-act(MAGICCUBE mc){
-	for (AXIS ax=X;;++ax){
-		if ax==father->axis && father->axis==father->father->axis
-			continue;
-		for (POS p=LOW;;++p){
-			if ax==father->axis && p==father->pos
-				continue;
-			for (DIR d=BACKWARD;;++d){
-				newmc = mc.rot(ax,p,d);
-				if newmc.successed()
-					newmc.print();
-				else
-					act(newmc);		
-			}
-		}
-	}
+	};
 };
 
 int
 main()
 {
-	MAGICCUBE mc = random();
-	act(mc);
+	MAGICCUBE mc;
+	mc.random();
+	mc.print();
+	mc->parent=null;
+	mc.act();
+	mc.print();
 	return 0;
 }
-
-
-
-
-	
-
